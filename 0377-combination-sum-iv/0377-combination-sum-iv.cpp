0d2d8ea91a523f789d0b35bd20a8e1c3eeebf3db
Viraj Sonaje <vsonaje@asu.edu>
2023-09-09 13:51:44 -0700

    Time: 3 ms (50.38%), Space: 6.7 MB (21.13%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<uint> dp(target+1, 0);
+
+        dp[0] = 1;
+        for(int i=0; i<=target; i++){
+            for(int num: nums){
+                if(num<=i){
+                    dp[i] = dp[i]+dp[i-num];
+                }
+            }
+        }
+        return dp[target];
+
+    }
+};
