912fcb6736300959d1d3b3c73389167bb5f8aa9f
Viraj Sonaje <vsonaje@asu.edu>
2024-07-10 23:19:18 -0700

    Time: 8 ms (32.92%), Space: 12.3 MB (93.83%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        if(n == 1) return {0, 1};
-        vector<int> ans(pow(2,n), 0);
-        ans[1] = 1;
-        for(int i=1; i<n; i++){
-            int offset = pow(2,i);
-            int idoffset = pow(2,i+1);
-            for(int id = 0; id < pow(2,i); id++){
-                ans[idoffset - 1 - id] = ans[id]+offset;
-            }
-        }
+        vector<int> ans(1<<n);
+        for (int i=0; i<(1<<n); i++) 
+            ans[i] = i^(i>>1);
         return ans;
     }
 };
