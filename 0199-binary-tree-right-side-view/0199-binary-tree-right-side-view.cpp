fa7b4261ceaba4638541882cab4e6cebeaf6831f
Viraj Sonaje <vsonaje@asu.edu>
2024-07-10 21:47:27 -0700

    Time: 3 ms (57.25%), Space: 14.1 MB (56.15%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(root == NULL) return {};
+        vector<int> ans;
+        queue<TreeNode*> level;
+        level.push(root);
+        levelOrder(ans, level);
+
+        return ans;
+    }
+
+    void levelOrder(vector<int> &lvlOrder, queue<TreeNode*> &level){ 
+        if(level.empty()) return;
+        int n = level.size();
+        cout<<n<<endl;
+        for(int i=0; i<n; i++){
+            TreeNode* node = level.front();
+            level.pop();
+            cout<<node->val<<" ";
+            if(i == n-1) lvlOrder.push_back(node->val);
+            if(node->left != NULL) level.push(node->left);
+            if(node->right != NULL) level.push(node->right);
+            
+        }
+        cout<<endl;
+
+        levelOrder(lvlOrder, level);
+    }
+};
