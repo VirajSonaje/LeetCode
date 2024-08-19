f56376670c588125712febf8a36f85f09aadfa8e
Viraj Sonaje <vsonaje@asu.edu>
2024-08-18 17:10:57 -0700

    Time: 0 ms (100%), Space: 9.8 MB (56.7%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        priority_queue<long long, vector<long long>, greater<long long>> pq;
-        set<long long> s;
-        pq.push(1);
+        vector<int> dp(n);
+        int p2 = 0, p3 = 0, p5 = 0;
+        dp[0] = 1;
         for(int i=1; i<n; i++){
-            long long cur = pq.top();
-            pq.pop();
-            if(s.find(cur*2) == s.end())s.insert(cur*2), pq.push(cur*2);
-            if(s.find(cur*3) == s.end())s.insert(cur*3), pq.push(cur*3);
-            if(s.find(cur*5) == s.end())s.insert(cur*5), pq.push(cur*5);
+            int mul2 = dp[p2]*2;
+            int mul3 = dp[p3]*3;
+            int mul5 = dp[p5]*5;
+
+            dp[i] = min(mul2, min(mul3, mul5));
+            if(dp[i] == mul2)p2++;
+            if(dp[i] == mul3)p3++;
+            if(dp[i] == mul5)p5++;
         }
 
-        return pq.top();
+        return dp[n-1];
     }
 };
