75e25bd2af85f2f60df522004afc3a62908b4919
Viraj Sonaje <vsonaje@asu.edu>
2024-10-05 15:26:43 -0700

    Time: 6 ms (68.06%), Space: 8.8 MB (80.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0; i<n; i++) s2map[s2[i]-'a']++;
         int l=0, r=n;
         while(r < s2.length()){
-            if(isSame(smap, s2map)) return true;
+            if(smap == s2map) return true;
             else{
                 s2map[s2[r]-'a']++;
                 s2map[s2[l]-'a']--;
@@ -16,14 +16,7 @@ public:
                 l++;
             }
         }
-        if(isSame(smap, s2map)) return true;
+        if(smap == s2map) return true;
         return false;
-    }
-
-    bool isSame(vector<int> a, vector<int> b){
-        for(int i=0; i<26; i++){
-            if(a[i] != b[i])return false;
-        }
-        return true;
     }
 };
