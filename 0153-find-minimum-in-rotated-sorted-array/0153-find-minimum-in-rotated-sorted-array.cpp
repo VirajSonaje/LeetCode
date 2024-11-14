cb3caa25735689f05f71aaf4c3d9c116e994a148
Viraj Sonaje <vsonaje@asu.edu>
2024-11-13 20:35:32 -0700

    Time: 0 ms (100%), Space: 12.9 MB (48.91%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,14 +4,11 @@ public:
         int left = 0;
         int right = nums.size()-1;
         int mid;
-        if (nums[left] < nums[right]) return nums[0];
         while(left < right){
             int mid = left + (right-left)/2;
-            if(mid == left || mid == right) break;
-            if(nums[mid] < nums[right]) right = mid;
-            else left = mid;
+            if(nums[mid] > nums[right]) left = mid+1;
+            else right = mid;
         }
-        cout<<nums[mid]<<" "<<nums[left]<<" "<<nums[right]<<endl;
-        return nums[right];
+        return nums[left];
     }
 };
