f3c6b744f7064ab6abfb6a0ce1065acf598725a1
Viraj Sonaje <vsonaje@asu.edu>
2024-11-13 19:56:12 -0700

    Time: 1792 ms (6.2%), Space: 284.6 MB (5.01%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    
-    bool dfs(int i, int j, vector<vector<char>> &board, string &word, int ind){
-        if(board[i][j] == '#') return false;
-        // vis[i][j] = true;
-        if(ind == word.length()) return true;
-        //cout<< board[i][j];
-        if(board[i][j] == word[ind]){
-            if(ind == word.length()-1) return true;
-            bool a = false;
-            board[i][j]='#';
-                if(i-1 >= 0) a = a||dfs(i-1,j, board, word, ind+1);
-                if(j-1>=0) a = a||dfs(i,j-1, board, word, ind+1);
-                if(i+1 < board.size()) a= a||  dfs(i+1,j, board, word, ind+1);
-                if(j+1 < board[0].size())a = a|| dfs(i,j+1, board, word, ind+1);
-            board[i][j]=word[ind];
-                return a;
-        }
-        
-        return false;
-    }
-
-
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>> vis(board.size(), vector<bool> (board[0].size(), false));
-        bool flag = false;
+        bool ans = false;
         for(int i=0; i<board.size(); i++){
             for(int j=0; j<board[0].size(); j++){
                 if(board[i][j] == word[0]){
-                    //cout<< board[i][j] << dfs(i,j, board, word, 0, vis);
-                    if(dfs(i,j, board, word, 0)) flag = true;
-                } 
-                
+                    if(search(board, word, 1, i, j)) return true;
+                }
             }
         }
-        return flag;
+
+        return false;
+    }
+
+    bool search(vector<vector<char>> &board, string word, int idx, int i, int j){
+        board[i][j] = '#';
+
+        if(idx == word.size()) return true;
+
+        vector<int> dirs = {1,-1};
+        bool ans = false;
+        for(int d : dirs){
+            if(i+d >=0 && i+d < board.size() && board[i+d][j] == word[idx])
+                ans |= search(board, word, idx+1, i+d, j);
+            if(ans) return ans;
+            if(j+d >=0 && j+d < board[0].size() && board[i][j+d] == word[idx])
+                ans|= search(board, word, idx+1, i, j+d);
+            if(ans) return ans;
+        }
+        board[i][j] = word[idx-1];
+        return ans;
     }
 };
