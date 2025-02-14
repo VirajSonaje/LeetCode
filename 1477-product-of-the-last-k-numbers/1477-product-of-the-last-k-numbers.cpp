f3bb9b2807052f6b0de277acc142456809a94bd7
Viraj Sonaje <vsonaje@asu.edu>
2025-02-14 00:32:39 -0700

    Time: 27 ms (43.54%), Space: 78.7 MB (12.53%) - LeetHub

---
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -0,0 +1,25 @@
+class ProductOfNumbers {
+    vector<long long> products;
+    long long currentProduct = 1;
+    long long totalProduct = 1;
+public:
+    void add(int num) {
+        if(num == 0){
+            products.erase(products.begin(), products.end());
+            currentProduct = 1;
+        }
+        else{
+            currentProduct*=1LL*num;
+            products.push_back(currentProduct);
+        }
+    }
+    
+    int getProduct(int k) {
+        if(k>products.size())
+            return 0;
+        else if(k == products.size())
+            return currentProduct;
+        long long productBeforeKNumbers = products[products.size() - k - 1];
+        return currentProduct / productBeforeKNumbers;
+    }
+};
