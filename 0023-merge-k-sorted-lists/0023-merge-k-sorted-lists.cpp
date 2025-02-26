19a910a26fcc88d5b435ee96981fa912cdafb08d
Viraj Sonaje <vsonaje@asu.edu>
2025-02-25 20:33:49 -0700

    Time: 128 ms (9.96%), Space: 18.1 MB (94.39%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -13,63 +13,47 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.size() == 0) return NULL;
 
-        while(lists.size()> 1){
-            lists[0] = mergeTwoLists(lists[0], lists[1]);
+        while(lists.size() > 1){
+            lists[0] = merge2Lists(lists[0], lists[1]);
             lists.erase(lists.begin()+1);
         }
         return lists[0];
     }
 
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        bool f1 = false, f2 = false;
+    ListNode* merge2Lists(ListNode* list1, ListNode* list2){
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+
         ListNode* head;
         ListNode* temp;
-        if(l1 == NULL){
-            return l2;
+        if(list1->val <= list2->val){
+            head = list1;
+            list1 = list1->next;
         }
-        if(l2 == NULL){
-            return l1;
-        }
-        if(l1->val <= l2->val){
-               head =l1;
-                l1= l1->next;
-           }
         else{
-            head = l2;
-            l2 = l2->next;
+            head = list2;
+            list2 = list2->next;
         }
         temp = head;
-       while(1){
-           
-           if(l1 == NULL){
-               f1=true;
-               temp->next= l2;
-               return head;
-           }
-           if(l2 == NULL){
-               f2 = true;
-               temp->next = l1;
-               return head;
-           }
-           if(f1 && f2){
-               return head;
-           }
-           if(l1->val <= l2->val){
-               
-               temp->next = l1;
-               l1 = l1->next;
-               temp = temp->next;
-               
-           }
-           else if(l2->val < l1->val){
-               
-               temp->next = l2;
-               temp = temp->next;
-               l2 = l2->next;
-               
-           }
-           
-       } 
-        
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+
+        if(list1 == NULL){
+            temp->next = list2;
+        }
+        if(list2 == NULL){
+            temp->next = list1;
+        }
+
+        return head;
     }
 };
