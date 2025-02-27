0a4379eefc5fd387b01678304722fceebb07a13c
Viraj Sonaje <vsonaje@asu.edu>
2025-02-26 22:40:26 -0700

    Time: 86 ms (54.13%), Space: 173.3 MB (62.48%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,17 +1,16 @@
 class LRUCache {
 public:
     list<pair<int, int>> cache;
-    map<int, list<pair<int, int>> :: iterator> lookup;
+    unordered_map<int, list<pair<int, int>>:: iterator> lookup;
     int capacity;
+
     LRUCache(int capacity) {
         this->capacity = capacity;
-        cache.clear();
-        lookup.clear();
     }
     
     int get(int key) {
         if(lookup.find(key) != lookup.end()){
-            cache.splice(cache.begin(),cache,lookup[key]);
+            cache.splice(cache.begin(), cache, lookup[key]);
             return lookup[key]->second;
         }
         return -1;
@@ -19,14 +18,13 @@ public:
     
     void put(int key, int value) {
         if(lookup.find(key) != lookup.end()){
-           cache.splice(cache.begin(),cache,lookup[key]);
+            cache.splice(cache.begin(), cache, lookup[key]);
             lookup[key]->second = value;
         }
         else{
-            if(cache.size()==this->capacity){
-                int k = cache.back().first;
-            cache.pop_back();
-            lookup.erase(k);
+            if(cache.size() == this->capacity){
+                lookup.erase(cache.back().first);
+                cache.pop_back();
             }
             cache.push_front({key, value});
             lookup[key] = cache.begin();
