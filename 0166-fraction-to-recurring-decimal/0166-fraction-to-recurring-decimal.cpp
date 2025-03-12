f044891314434dbfb92aa5422c447e2c471eef90
Viraj Sonaje <vsonaje@asu.edu>
2025-03-12 07:38:02 -0700

    Time: 0 ms (100%), Space: 9.4 MB (70.93%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     string fractionToDecimal(int numerator, int denominator) {
-        if(numerator == 0)return "0";
-        string ans = "";
+        if(numerator == 0) return "0";
         long rem = numerator;
-        long quo = 0;
+        long quo =0;
         long den = denominator;
+        string ans = "";
+        if(rem < 0 ^ den <0) ans+="-";
+        rem = abs(rem);
+        den = abs(den);
         quo = rem/den;
         rem %= den;
-        ans+=to_string(quo);
-        if(!rem) return ans;
+        if(rem == 0) return ans+to_string(quo);
+        ans += to_string(quo);
         ans+=".";
-        bool flag = true;
+        rem*=10;
         unordered_map<long, int> mp;
-        while(rem){
+        while(rem > 0){
             if(mp.count(rem) > 0){
                 ans.insert(mp[rem], 1, '(');
                 ans+=')';
-                break;
+                return ans;
             }
-
             mp[rem] = ans.length();
+            quo = rem/den;
+            ans+=to_string(quo);
+            rem %= den;
             rem*=10;
-            ans+=to_string(rem/den);
-            rem%=den;
         }
         return ans;
     }
