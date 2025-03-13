bcc91e64bb3608f9e979d3b9e19745fa7054b38e
Viraj Sonaje <vsonaje@asu.edu>
2025-03-12 21:56:28 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        vector<int> dp(s.length()+1, 0);
         int n = s.length();
-        if(s[0]=='0') return 0;
+        vector<int> dp(n+1, 0);
+
+        if(s[0] == '0') return 0;
         dp[0] = 1;
-
-        //if(n ==1) return dp[0];
-
         dp[1] = 1;
-        for(int i = 2; i<=n; i++){
-            if(s[i-1]>='1' && s[i-1] <= '9'){
-                dp[i] += dp[i-1];
-            }
-            string a = s.substr(i-2,2);
-            if(stoi(a) <= 26 &&  stoi(a) >= 10){
+        for(int i=2; i<=n;i++){
+            int dw = stoi(s.substr(i-2, 2));
+            int single = stoi(s.substr(i-1,1));
+            if(single >0) dp[i]+=dp[i-1];
+            if(dw <= 26 && s[i-1] != 0){
                 dp[i] += dp[i-2];
             }
         }
