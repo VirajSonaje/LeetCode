9fe247830adee304e228b0ba733116afab19ac91
Viraj Sonaje <vsonaje@asu.edu>
2025-03-15 12:48:58 -0700

    Time: 44 ms (11.11%), Space: 115.1 MB (5.31%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int left =1;
+        int right = *max_element(nums.begin(), nums.end());
+        int ans =right;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(satisfy(nums,k, mid)){
+                ans = mid;
+                right = mid-1;
+            }
+            else left = mid+1;
+        }
+        return ans;
+    }
+
+    bool satisfy(vector<int> nums, int k, int mid){
+        int count =0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] <= mid){
+                count++;
+                i++;
+            }
+        }
+        return count >= k;
+    }
+};
