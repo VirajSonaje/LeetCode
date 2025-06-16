3a1177c31e10bbbd792fe2b9cab77a1b7cabba53
Viraj Sonaje <vsonaje@asu.edu>
2025-06-15 17:26:16 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        return helper(s, 0);
+    }
+
+    string helper(string& s, int &i){
+        string ans; 
+        while(i<s.length() && s[i] != ']'){
+            cout<<i<<endl;
+            if(!isdigit(s[i])){
+                ans+=s[i];
+                i++;
+            }
+            else{
+                int n=0; 
+                while(i < s.length() && isdigit(s[i])){
+                    n = n*10 + s[i]-'0';
+                    i++;
+                }
+                i++; 
+                string t = helper(s, i);
+                i++;
+
+                while(n-- > 0){
+                    ans+=t;
+                }
+            }
+        }
+        return ans;
+    }
+};
